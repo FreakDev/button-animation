220b378f18fbf196a1b62471a44790e5b3b1a6ed
Mathias Desloges <freakdev@gmail.com>
2018-03-27 17:44:16 +0200

    create a specific file for component

---
+++ src/ButtonAnimation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './index.css';
+
+export default class ButtonAnimation extends React.Component {
+
+    handleClick(){
+        console.log('clicked')
+        var startAnimate = document.getElementById('btn');
+        var circle = document.getElementById('cl');
+        startAnimate.classList.add('reduce-width');
+        circle.classList.add('progress-circle');
+    }
+    render() {
+        return (
+            <div id="div"> 
+                    <button id="btn" onClick={this.handleClick}>submit </button>
+                    <svg id="cl" className="circle" width="70" height="70">
+                        <path d="m35,2.5c17.955803,0 32.5,14.544199 32.5,32.5c0,17.955803 -14.544197,32.5 -32.5,32.5c-17.955803,0 -32.5,-14.544197 -32.5,-32.5c0,-17.955801 14.544197,-32.5 32.5,-32.5z"/>
+                    </svg>
+            </div>
+        );    
+    }
+}
